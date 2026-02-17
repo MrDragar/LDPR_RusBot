31e41ad4124fc81f2acf040479f1a6cdbf462748
mrdragar <yshhenyaev@mail.ru>
2026-02-17 19:22:55 +0300

    Change miniapp to website

--- tg_bot/src/application/keyboards/miniapp_keyboard.py
+++ tg_bot/src/application/keyboards/miniapp_keyboard.py
@@ -4,5 +4,5 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 def get_miniapp_keyboard() -> InlineKeyboardMarkup:
     keyword = InlineKeyboardBuilder()
-    keyword.button(text="Открыть сайт", web_app=WebAppInfo(url="https://ldpr.ru"))
+    keyword.button(text="Открыть сайт", url="https://ldpr.ru")
     return keyword.as_markup()
