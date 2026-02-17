9eeb3725b14fa67ce27b83a82d83937c0c0a44fb
mrdragar <yshhenyaev@mail.ru>
2026-02-17 19:16:55 +0300

    Change link

--- vk_bot/src/application/keyboards/miniapp_keyboard.py
+++ vk_bot/src/application/keyboards/miniapp_keyboard.py
@@ -3,4 +3,4 @@ from vkbottle import Keyboard, OpenLink
 
 def get_miniapp_keyboard():
     # В VK Mini App открывается через ссылку vk.com/app...
-    return Keyboard(inline=True).add(OpenLink("https://плюссемь.рф", "Открыть сайт")).get_json()
+    return Keyboard(inline=True).add(OpenLink("https://ldpr.ru/", "Открыть сайт")).get_json()
